6228b48bd53988d306b776a5bba1d18bdb840018
RobertoMartins <robertomurilocordeiro@gmail.com>
2023-11-17 13:09:39 -0300

    feat: create Controller Exception Handler

---
+++ src/main/java/com/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.picpaysimplificado.infra;
+
+import javax.swing.text.html.parser.Entity;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.picpaysimplificado.dtos.ExceptionDTO;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ExceptionDTO> threatDuplicateEntity(DataIntegrityViolationException exception) {
+
+        ExceptionDTO responseException = new ExceptionDTO("Usuário já cadastrado", "400");
+        return ResponseEntity.badRequest().body(responseException);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionDTO> threatNotFoundEntity(DataIntegrityViolationException exception) {
+
+        ExceptionDTO responseException = new ExceptionDTO("Usuário não encontrado", "400");
+        return ResponseEntity.badRequest().body(responseException);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionDTO> threatGeneralException(Exception exception) {
+
+        ExceptionDTO responseException = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(responseException);
+    }
+
+}
